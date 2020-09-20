035ae274d42cb9a39481825f4fdb780307c5cc59
gaurav1781 <gaurav145@gmail.com>
2020-09-20 16:56:52 +1000

    Add files via upload

---
+++ First/src/second.java
@@ -0,0 +1,48 @@
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.FileInputStream;
+import java.util.List;
+
+public class second
+{
+    public static void main(String[] args) throws Exception
+    {
+		/*int g=10;
+		System.out.print(g);*/
+        System.setProperty("webdriver.gecko.driver","D:\\Selenium\\Drivers\\geckodriver.exe");
+        System.setProperty("webdriver.chrome.driver","D:\\Selenium\\Drivers\\chromedriver.exe");
+        /*FirefoxDriver f1=new FirefoxDriver();*/
+        ChromeDriver f1=new ChromeDriver();
+        f1.get("http://mail.yahoo.com");
+        FileInputStream file1=new FileInputStream("D:\\Selenium\\First.xlsx");
+        XSSFWorkbook wb=new XSSFWorkbook(file1);
+        XSSFSheet sheet=wb.getSheetAt(0);
+        //	Row row=sheet.getRow(0);
+        //Cell cell=row.getCell(0);
+        f1.findElementByXPath("//*[@id=\"identifierId\"]").sendKeys(sheet.getRow(1).getCell(0).toString());
+        f1.findElementByXPath("//*[@id=\"identifierNext\"]").click();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+        f1.findElementByXPath("//*[@id=\"password\"]/div[1]/div/div[1]/input").sendKeys(sheet.getRow(1).getCell(1).toString());
+        f1.findElementByXPath("//*[@id=\"passwordNext\"]").click();
+        f1.manage().window().maximize();
+        List<WebElement> l1=f1.findElements(By.className("bqe"));
+        for (WebElement myelement : l1)
+        {
+            String link=myelement.getText();
+            System.out.println(link);
+            /*System.out.println(myelement);*/
+        }
+        /*f1.findElementByXPath("//*[@id=\"gb\"]/div[2]/div[3]/div/div[2]/div/a/span").click();*/
+        f1.findElementByXPath("//span[@class='gb_ab gbii']").click();
+        f1.findElementById("gb_71").click();
+    }
+}
